4bac2886e6c0999ea4a203fb5c3ed6408f99ee74
Giveonaldo <giveonaldo@gmail.com>
2022-12-19 21:40:57 +0700

    Code V1.1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         public string name;
         public Node next;
     }
+
+    class Queue
+    {
+        Node abriansyah, adam; // Untuk abriansyah menggunakan nama depan dan adam menggunakan nama belakang.
+        public Queue()
+        {
+            abriansyah = null;
+            adam = null;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
